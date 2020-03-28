482f237c05b480fb6183c3bc0fe7a38ef4862405
LT <jackaroo1024@163.com>
2020-03-28 15:07:12 +0800

    递归函数

---
+++ src/c01/Demo.java
@@ -0,0 +1,21 @@
+package c01;
+/**
+ * @description  描述
+ *
+ * @author  LT
+ * @date  2020/3/28
+ */
+public class Demo {
+
+    public static int fact(int n){
+        if (n ==1) {
+            return  1;
+        }
+        return n*fact(n-1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+}

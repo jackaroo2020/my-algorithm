f042dd93c5ff9e1dac62a95d884673fbeea17f4c
LT <jackaroo1024@163.com>
2020-07-14 08:08:56 +0800

    最小倍数和所有非盈数之和

---
+++ src/day/Day03.java
@@ -0,0 +1,12 @@
+package day;
+/**
+ * @description  最小倍数
+ * 2520是最小的能够被1到10整除的数。
+ * 最小的能够被1到20整除的正数是多少？
+ * 思想：
+ *
+ * @author  LT
+ * @date  2020/7/14
+ */
+public class Day03 {
+}
---
+++ src/day/Day04.java
@@ -0,0 +1,9 @@
+package day;
+/**
+ * @description  描述
+ *
+ * @author  LT
+ * @date  2020/7/14
+ */
+public class Day04 {
+}
